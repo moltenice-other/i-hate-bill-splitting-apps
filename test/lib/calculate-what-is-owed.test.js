06249b1697015eaa23b74fc50993f40fda4e67e9
Karan Shah <dabrainstabber@gmail.com>
2020-08-12 00:05:31 +0100

    Added logic to convert from a string of entries to an array of entries

--- test/lib/calculate-what-is-owed.test.js
+++ test/lib/calculate-what-is-owed.test.js
@@ -1,7 +1,17 @@
-const { getExpression } = require('../../lib/calculate-what-is-owed')
+const { getExpression, getEntries } = require('../../lib/calculate-what-is-owed')
+const outdent = require('outdent')
 
-test('I can get the expression to calculate from the String', () => {
-  const input = '£100.00/2 for electricity'
-  const output = getExpression(input)
+test('I can get the expression to calculate from the entry', () => {
+  const entry = '£100.00/2 for electricity'
+  const output = getExpression(entry)
   expect(output).toBe('100.00/2')
+})
+
+test('I can get an array of entries from a String of entries', () => {
+  const input = outdent`
+    £100.00/2 for electricity
+    £20 for groceries
+  `
+  const output = getEntries(input)
+  expect(output).toStrictEqual(['£100.00/2 for electricity', '£20 for groceries'])
 })
